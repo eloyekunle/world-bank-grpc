35888c1066e4bb48c9c0c161b7c7dff6a4742b72
Elijah Oyekunle <eloyekunle@gmail.com>
2020-03-31 00:59:39 +0100

    add test for get country

--- pkg/mock_worldbank/wb_mock_test.go
+++ pkg/mock_worldbank/wb_mock_test.go
@@ -1,1 +1,57 @@
-package mock_worldbank
+package mock_worldbank_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	wbmock "github.com/eloyekunle/world-bank-grpc/pkg/mock_worldbank"
+	"github.com/eloyekunle/world-bank-grpc/pkg/worldbank"
+	"github.com/golang/mock/gomock"
+	"github.com/golang/protobuf/proto"
+)
+
+// rpcMsg implements the gomock.Matcher interface
+type rpcMsg struct {
+	msg proto.Message
+}
+
+func (r *rpcMsg) Matches(msg interface{}) bool {
+	m, ok := msg.(proto.Message)
+	if !ok {
+		return false
+	}
+	return proto.Equal(m, r.msg)
+}
+
+func (r *rpcMsg) String() string {
+	return fmt.Sprintf("is %s", r.msg)
+}
+
+func TestGetCountry(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockWorldBankClient := wbmock.NewMockWorldBankClient(ctrl)
+	req := &worldbank.CountryID{Id: "NGA"}
+
+	mockWorldBankClient.EXPECT().GetCountry(
+		gomock.Any(),
+		&rpcMsg{msg: req},
+	).Return(&worldbank.Country{Id: "NGA", Name: "Nigeria", Capital: "Abuja"}, nil)
+
+	testGetCountry(t, mockWorldBankClient)
+}
+
+func testGetCountry(t *testing.T, client worldbank.WorldBankClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := client.GetCountry(ctx, &worldbank.CountryID{Id: "NGA"})
+	if err != nil || r.Capital != "Abuja" {
+		t.Errorf("mocking failed")
+	}
+
+	t.Log("Reply : ", r.Capital)
+}
